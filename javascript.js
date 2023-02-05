d40f326b8ff452a74223de7ddcb37a26ab5120b1
Farmata <kane.farmata@gmail.com>
2023-02-05 22:08:13 +0000

    Get getComputerChoice and playRound functions up and running

---
+++ javascript.js
@@ -0,0 +1,33 @@
+
+// create a function that randomly return either 'rock, paper or scissor'
+function getComputerChoice(choice) {
+    choice = ["Rock", "Paper", "Scissor"];
+    const randomChoice = Math.floor(Math.random() * choice.length);
+    return choice[randomChoice].toString();
+}
+
+// create a function that plays 1 round with 2 par: playerSelection and computerSelection
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return console.log("It\'s a tie!");
+        } else if (playerSelection === "rock" && computerSelection === "Scissor") {
+            return console.log("You win! Rock destroys Scissor");
+        } else if (playerSelection === "scissor" && computerSelection === "Paper") {
+            return console.log("You win! Scissor cuts Paper");
+        } else if (playerSelection === "paper" && computerSelection === "Rock") {
+            return console.log("You win! Paper covers Rock");
+        } else {
+            return console.log(`You loose, The Computer overpowered you with ${computerSelection}`);
+        }  
+    } 
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Rock, paper or scissor:");
+console.log(playRound(playerSelection, computerSelection));
+
+// return a string that declares the winner of the round 'You lose! Paper beats Rock'
+// user input should be case-insensitive
+
+// rock > scissor
+// scissor > paper
+// paper > rock
